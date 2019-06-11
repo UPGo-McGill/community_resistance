05b22f50876b6cf2f0990f9cad9427135b88cd3c
amy <amy.bolt@mail.mcgill.ca>
2019-06-11 11:58:48 -0400

    set up so that you just hvae to enter neighbourhood names and import the right file

--- R/02_data_import.R
+++ R/02_data_import.R
@@ -1,29 +1,44 @@
 ############## DATA IMPORT ###############################
 source("R/01_helper_functions.R")
 
-community_resistance_words = c("protest", "anti", "community-led", "affordability", 
-                               "oppose",  "resist", "resistance")
-
-# how to do this so all you have to input is city name and string of neighbourhood names
-# from one data set? all articles about airbnb from whichever newssources - filter down by city name?
-
+# Set up neighbourhood names
+neighbourhoods_tidy <- rbind(
+  data_frame(neighbourhood = "williamsburg", names = c("williamsburg")),
+  data_frame(neighbourhood = "park-slope", names = c("slope")),
+  data_frame(neighbourhood = "bedford-stuyvesant", names = c("bedford", "stuyvesant", "bed-stuy", "bedstuy", "stuy")),
+  data_frame(neighbourhood = "crown heights", names = c("crown")),
+  data_frame(neighbourhood = "flatbush", names = c("flatbush")),
+  data_frame(neighbourhood = "dumbo", names = c("dumbo")),
+  data_frame(neighbourhood = "bushwick", names = c("bushwick")))
+  
 # Import txt file with airbnb + city name
-# need to create a tidy table with the names and then the breakdowns and somehow incorporate that 
-neighbourhoods <- c("williamsburg", "slope", "bedford", "bed-stuy", "stuyvesant", 
-                    "crown", "flatbush", "dumbo", "bushwick")
-
 city <- lnt_read("txt_files/airbnb_williamsburg.TXT") %>% 
   lnt_convert(to = "tidytext")
 
 # Create a data table that calculates number of mentions and number of those that represent community opposition
 n = 1
 
+neighbourhoods <- 
+  neighbourhoods_tidy$neighbourhood %>% 
+  unique()
+
 neighbourhood_resistance <- tibble(neighbourhoods = character(0), mentions = numeric(0), opposition = numeric(0)) 
+
+community_resistance_words = c("protest", "anti", "community-led", "affordability", 
+                               "oppose",  "resist", "resistance", "opposition", "gentrification", 
+                               "threat", "threatening", "manifestation", "complaint", "disapprove",
+                               "evict", "eviction", "overtourism", "detriment", "detrimental", "ghost",
+                               "consultation", "opponent", "opponents", "discrimination", "critic", 
+                               "critics", "crisis", "shortage")
+
+# incorporate new neighbourhood table somehow (for second and third column)
 
 repeat{
   neighbourhood_resistance[n, 1] <- neighbourhoods[n]
   neighbourhood_resistance[n,2] <- city %>% 
-    filter(Token == neighbourhoods[n]) %>% 
+    filter(Token %in% 
+             (filter(neighbourhoods_tidy, neighbourhood == neighbourhoods [n]) %>% 
+                         pull(names))) %>% 
     select("ID") %>% 
     distinct() %>% 
     nrow()
@@ -32,7 +47,9 @@ repeat{
     filter(Token %in% community_resistance_words) %>% 
     select("ID") %>% 
     inner_join(city %>% 
-                 filter(Token == neighbourhoods[n]) %>% 
+                 filter(Token %in% 
+                          (filter(neighbourhoods_tidy, neighbourhood == neighbourhoods [n]) %>% 
+                                  pull(names))) %>% 
                  select("ID") %>% 
                  distinct(), .) %>% 
     distinct() %>% 
@@ -44,6 +61,4 @@ repeat{
     break
   }
 }
-
-
 
