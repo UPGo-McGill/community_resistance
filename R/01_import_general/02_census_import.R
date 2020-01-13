9c1cb9bc7ebdf8d5d08b8b71718f9db93fddfc48
amy <amy.bolt@mail.mcgill.ca>
2020-01-13 14:04:43 -0500

    st_transform

--- R/01_import_general/02_census_import.R
+++ R/01_import_general/02_census_import.R
@@ -19,7 +19,7 @@ canada <-
                 "v_CA16_4837", "v_CA16_4838", "v_CA16_512", 
                 "v_CA16_3393", "v_CA16_3996"),
     geo_format = "sf") %>% 
-  st_transform(3347) %>% 
+  st_transform(32618) %>% 
   select(GeoUID, Population, Households, contains("v_CA"))
 
 names(canada) <- 
