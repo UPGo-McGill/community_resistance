79cefe8743053e5a5c9bc31ea44d6526a16a791a
amy <amy.bolt@mail.mcgill.ca>
2020-01-22 15:10:54 -0500

    reorg

--- R/02_import_cities/04_media_import.R
+++ R/02_import_cities/04_media_import.R
@@ -2,27 +2,32 @@
 
 source("R/01_import_general/01_helper_functions.R")
 
-cityname <- "montreal"
+# Import factiva and lexisnexis files
 
-# Import factiva files
+cityname <- c("montreal", "toronto")
 
-media_FTV <- import_factiva(cityname)
+media <- 
+  
+  map(cityname, ~{
+    
+    rbind(import_factiva(.x), 
+          import_lexisnexis(.x)) %>% 
+      dplyr::select(1:9) %>% 
+      group_by(Author) %>% 
+      distinct(Headline, .keep_all = TRUE) %>% 
+      ungroup()
+    
+  })
 
-# Import lexisnexis files
 
-media_LN <- import_lexisnexis(cityname)
-
-# Merge files
-
-media <- rbind(media_FTV, media_LN) %>% 
-  dplyr::select(1:9) %>% 
-  group_by(Author) %>% 
-  distinct(Headline, .keep_all = TRUE) %>% 
-  ungroup()
 
 # Tidy text 
 
-list <- str_tidytext(media)
+  # how to save with unique names 
+
+list <- str_tidytext(media[[1]])
 media <- list [[1]]
 lemmatized_articles <- list [[2]]
+
+
 
