ffcc60c58e784bf542445805e4d11d6464dd5110
amy <amy.bolt@mail.mcgill.ca>
2020-01-30 14:38:25 -0500

    data import complete EXCEPT principal residence

--- R/07_airbnb_data_import_INCOMPLETE.R
+++ R/07_airbnb_data_import_INCOMPLETE.R
@@ -79,112 +79,68 @@ LTM_property <-
 # Process multilistings
 
 EH_ML <- 
-  ML_daily %>% 
-  filter(listing_type == "Entire home/apt") %>% 
-  group_by(listing_type, host_ID, date) %>% 
-  count() %>% 
-  ungroup() %>% 
-  filter(n >= 2) %>% 
-  mutate(ML = TRUE)
+  map(ML_daily, ~{
+    .x %>% 
+      filter(listing_type == "Entire home/apt") %>% 
+      group_by(listing_type, host_ID, date) %>% 
+      count() %>% 
+      ungroup() %>% 
+      filter(n >= 2) %>% 
+      mutate(ML = TRUE)
+  })
+ 
+PR_ML <- 
+  map(ML_daily, ~{
+    .x %>% 
+      filter(listing_type == "Private room") %>% 
+      group_by(listing_type, host_ID, date) %>% 
+      count() %>% 
+      ungroup() %>% 
+      filter(n >= 3) %>% 
+      mutate(PR_ML = TRUE)
+  })
 
+daily <- 
+  map(seq_along(EH_ML), ~{
+    EH_ML[[.x]] %>% 
+      dplyr::select(-n) %>% 
+      left_join(daily[[.x]], .)
+  })
 
+daily <- 
+  map(seq_along(PR_ML), ~{
+    PR_ML[[.x]] %>% 
+      dplyr::select(-n) %>% 
+      left_join(daily[[.x]], .) %>% 
+      mutate(ML = if_else(is.na(ML), PR_ML, ML)) %>% 
+      mutate(ML = if_else(is.na(ML), FALSE, ML)) %>% 
+      dplyr::select(-PR_ML)
+  })
 
-# ADD PRINCIPAL RESIDENCE FUNCTION
+rm(EH_ML, PR_ML)
 
+# Calculate FREH and GH listings
 
+FREH <- 
+  map(daily, ~{
+    .x %>% 
+      strr_FREH("2017-01-01", end_date) %>% 
+      filter(FREH == TRUE) %>% 
+      dplyr::select(-FREH)
+  })
 
+GH <- map(property, ~{
+  .x %>% 
+    strr_ghost(start_date = "2017-01-01", end_date = end_date)
+})
 
-# Expand the daily file
-daily <- daily %>% 
-  strr_expand_daily()
+# Calculate principal residence fields
+  # WAITING FOR FUNCTION TO BE COMPLETE
+property_subset <- 
+  map(seq_along(property_subset), ~{
+    property[[.x]] %>% 
+      strr_principal_res(daily[[.x]], FREH[[.x]], GH[[.x]], 
+                               start_date, end_date)
+  })
 
-# Set columns, select housing, and transform geometries
-property <- property %>% 
-  select(c("property_ID", "listing_title", "property_type", "listing_type",
-           "created", "scraped", "latitude", "longitude", "ab_property",
-           "ab_host", "ha_property", "ha_host")) %>% 
-  set_names(c("Property_ID", "Listing_Title", "Property_Type", "Listing_Type",
-              "Created", "Scraped", "Latitude", "Longitude", "Airbnb_PID",
-              "Airbnb_HID", "HomeAway_PID", "HomeAway_HID")) %>% 
-  arrange(Property_ID) %>% 
-  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
-  st_transform(transform) %>% 
-  filter(Property_Type %in% c(
-    "House", "Private room in house", "Apartment", "Cabin",
-    "Entire condominium", "Townhouse", "Condominium", "Entire apartment",
-    "Private room", "Loft", "Place", "Entire house", "Villa", "Guesthouse",
-    "Private room in apartment", "Guest suite", "Shared room in dorm",
-    "Chalet", "Dorm", "Entire chalet", "Shared room in loft", "Cottage",
-    "Resort", "Serviced apartment", "Other", "Bungalow", "Farm stay",
-    "Private room in villa", "Entire loft", "Entire villa",
-    "Private room in guesthouse", "Island", "Entire cabin", "Vacation home",
-    "Entire bungalow", "Earth house", "Nature lodge", "In-law",
-    "Entire guest suite", "Shared room in apartment", "Private room in loft",
-    "Tiny house", "Castle", "Earth House", "Private room in condominium",
-    "Entire place", "Shared room", "Hut", "Private room in guest suite",
-    "Private room in townhouse", "Timeshare", "Entire townhouse",
-    "Shared room in house", "Entire guesthouse", "Shared room in condominium",
-    "Cave", "Private room in cabin", "Dome house",
-    "Private room in vacation home", "Private room in dorm",
-    "Entire serviced apartment", "Private room in bungalow",
-    "Private room in serviced apartment", "Entire Floor", "Entire earth house",
-    "Entire castle", "Shared room in chalet", "Shared room in bungalow",
-    "Shared room in townhouse", "Entire cottage", "Private room in castle",
-    "Private room in chalet", "Private room in nature lodge", "Entire in-law",
-    "Shared room in guesthouse", "Casa particular", "Serviced flat", "Minsu",
-    "Entire timeshare", "Shared room in timeshare", "Entire vacation home",
-    "Entire nature lodge", "Entire island", "Private room in in-law",
-    "Shared room in serviced apartment", "Shared room in cabin", "Entire dorm",
-    "Entire cave", "Private room in timeshare", "Shared room in guest suite",
-    "Private room in cave", "Entire tiny house",
-    "Private room in casa particular (cuba)", "Casa particular (cuba)",
-    "Private room in cottage", "Private room in tiny house",
-    "Entire casa particular", "")) %>% 
-  select(-Property_Type)
-
-daily <- daily %>% 
-  select(c("property_ID", "date", "status", "price")) %>%
-  set_names(c("Property_ID", "Date", "Status", "Price")) %>%
-  filter(!is.na(Status)) %>%
-  arrange(Property_ID, Date)
-  
-# Trim listings
-property <-
-  property %>% 
-  filter(Property_ID %in% daily$Property_ID)
-
-# Join property and daily file
-daily <- inner_join(daily, st_drop_geometry(property)) %>% 
-  select(Property_ID, Date, Status, Price, Airbnb_PID, HomeAway_PID, Airbnb_HID,
-         HomeAway_HID, Listing_Type, Created, Scraped)
-
-daily <- daily %>% 
-  filter(Date >= Created, Date <= Scraped + 30)
-
-# Find FREH listings
-daily_FREH <- strr_FREH(daily, start_date = end_date, end_date = end_date)
-
-daily_FREH <- daily_FREH %>% 
-  filter(FREH == TRUE) %>% 
-  select(Property_ID) %>% 
-  distinct()
-
-property <- property %>% 
-  mutate(FREH = property$Property_ID %in% daily_FREH$Property_ID)
-
-rm(daily_FREH)
-
-# Identify ghost hotels
-GH_list <-
-  strr_ghost(property, Property_ID, Airbnb_HID, Created, Scraped, start_date,
-             end_date, listing_type = Listing_Type) %>% 
-  pull(property_IDs) %>%
-  unlist() %>%
-  unique()
-
-property <-
-  property %>% 
-  mutate(GH = if_else(Property_ID %in% GH_list, TRUE, FALSE))
-
-rm(GH_list)
 
