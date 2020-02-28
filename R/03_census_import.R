97f7a3e0b433ad77c043262e0aec941417600dba
amy <amy.bolt@mail.mcgill.ca>
2020-02-28 14:36:07 -0500

    remvoing canadian cities

--- R/03_census_import.R
+++ R/03_census_import.R
@@ -4,150 +4,149 @@ source("R/01_helper_functions.R")
 
 # Census import for all census tracts in Canada and the census tracts in the US in the following states
 states <- unique(fips_codes$state)[1:51]
-states = c("NY", "CA", "FL", "DC", "LA")
-
-########################################### 1 - CANADA #######################################
-
-# 1.1 LIST CENSUS VARIABLES
-variables_canada <- list_census_vectors("CA16")
-
-# 1.2 IMPORT CENSUS VARIABLES FOR COUNTRY-WIDE AVERAGE
-canada <-
-  get_census(
-    dataset = "CA16", regions = list(C = "Canada"),
-    vectors = c("v_CA16_2398", "v_CA16_5078", "v_CA16_4888", "v_CA16_6695",
-                "v_CA16_4837", "v_CA16_4838", "v_CA16_524", 
-                "v_CA16_3393", "v_CA16_3996", "v_CA16_2540", "v_CA16_497", "v_CA16_484"),
-    geo_format = "sf") %>% 
-  st_transform(102009) %>% 
-  dplyr::select(GeoUID, Population, Households, contains("v_CA"))
-
-names(canada) <- 
-  c("Geo_UID", "population", "households", "med_income",
-    "university_education", "housing_need", "non_mover", "owner_occupied", 
-    "rental", "no_language", "citizen", "white", "low_income_pct_pop", "lone_parent", 
-    "families", "geometry")
-
-canada <- canada %>% 
-  mutate(language = population - no_language)
-
-canada <- canada %>% 
-  mutate(
-    lone_parent_pct_families = lone_parent / families,
-    low_income = low_income_pct_pop/100*population,
-    low_income_pct_pop = low_income_pct_pop/100,
-    language = population - no_language) %>% 
-  mutate_at(
-    .vars = c("university_education", "non_mover", 
-              "language", "citizen", "white"),
-    .funs = list(`pct_pop` = ~{. / population})) %>% 
-  mutate_at(
-    .vars = c("housing_need", "owner_occupied", "rental"),
-    .funs = list(`pct_household` = ~{. / households}))
-
-
-# 1.3 IMPORT CENSUS VARIABLES FOR ALL CMAs
-CMAs_canada <-
-  get_census(
-    dataset = "CA16", regions = list(C = "Canada"), level = "CMA",
-    vectors = c("v_CA16_2398", "v_CA16_5078", "v_CA16_4888", "v_CA16_6695",
-                "v_CA16_4837", "v_CA16_4838", "v_CA16_524", 
-                "v_CA16_3393", "v_CA16_3996", "v_CA16_2540", "v_CA16_497", "v_CA16_484"),
-    geo_format = "sf") %>% 
-  st_transform(102009) %>% 
-  filter(Type == "CMA") %>% 
-  dplyr::select(GeoUID, CMA_name = name, Population, Households, contains("v_CA")) 
-
-CMAs_canada$CMA_name <- CMAs_canada$CMA_name %>% 
-  iconv(to = "ASCII//TRANSLIT") %>% 
-  str_replace("'", "") %>% 
-  str_replace("`", "")
-
-names(CMAs_canada) <- 
-  c("CMA_UID", "CMA_name", "population", "households", "med_income",
-    "university_education", "housing_need", "non_mover", "owner_occupied", 
-    "rental", "no_language", "citizen", "white", "low_income_pct_pop", "lone_parent",
-    "families", "geometry")
-
-CMAs_canada <- CMAs_canada%>% 
-  separate(CMA_name, into = c("CMA_name", NA), sep = "[(]") %>% 
-  mutate(
-    lone_parent_pct_families = lone_parent / families,
-    low_income = low_income_pct_pop/100*population,
-    low_income_pct_pop = low_income_pct_pop/100,
-    language = population - no_language) %>% 
-  mutate_at(
-    .vars = c("university_education", "non_mover", 
-              "language", "citizen", "white"),
-    .funs = list(`pct_pop` = ~{. / population})) %>% 
-  mutate_at(
-    .vars = c("housing_need", "owner_occupied", "rental"),
-    .funs = list(`pct_household` = ~{. / households}))
-
-CMAs_canada$CMA_name <- substr(CMAs_canada$CMA_name, 1, nchar(CMAs_canada$CMA_name) - 1)
-
-# 1.4 IMPORT CENSUS VARIABLES FOR ALL CENSUS TRACTS
-CTs_canada <-
-  get_census(
-    dataset = "CA16", regions = list(C = "Canada"), level = "CT",
-    vectors = c("v_CA16_2398", "v_CA16_5078", "v_CA16_4888", "v_CA16_6695",
-                "v_CA16_4837", "v_CA16_4838", "v_CA16_524", 
-                "v_CA16_3393", "v_CA16_3996", "v_CA16_2540", "v_CA16_497", "v_CA16_484"),
-    geo_format = "sf") %>% 
-  st_transform(102009) %>% 
-  filter(Type == "CT") %>% 
-  dplyr::select(GeoUID, PR_UID, CMA_UID, Population, Households, contains("v_CA"))
-
-CTs_canada <- CTs_canada %>% 
-  inner_join(st_drop_geometry(dplyr::select(CMAs_canada, c(1,2))), by = "CMA_UID") %>% 
- dplyr::select(GeoUID, CMA_UID, CMA_name, everything()) 
-
-names(CTs_canada) <- 
-  c("Geo_UID", "CMA_UID", "CMA_name", "PR_UID", "population", "households", "med_income",
-    "university_education", "housing_need", "non_mover", "owner_occupied", 
-    "rental", "no_language", "citizen", "white", "low_income_pct_pop",
-    "lone_parent", "families", "geometry")
-
-CTs_canada <- CTs_canada%>% 
-  mutate(
-    lone_parent_pct_families = lone_parent / families,
-    low_income = low_income_pct_pop/100*population,
-    low_income_pct_pop = low_income_pct_pop/100,
-    language = population - no_language) %>% 
-  mutate_at(
-    .vars = c("university_education", "non_mover", 
-              "language", "citizen", "white"),
-    .funs = list(`pct_pop` = ~{. / population})) %>% 
-  mutate_at(
-    .vars = c("housing_need", "owner_occupied", "rental"),
-    .funs = list(`pct_household` = ~{. / households})) %>% 
-  dplyr::select(-c("no_language"))
-
-
-# 1.5 Z SCORES
-datalist = list()
-
-for (n in c(1:nrow(CMAs_canada))) {
-  
-  CTs_canada_temp <- CTs_canada %>% 
-    filter(CMA_UID == as.numeric(st_drop_geometry(CMAs_canada[n, 1]))) %>% 
-    mutate_at(
-      .vars = c("population", "households", "med_income",
-                "university_education", "housing_need", "non_mover", "owner_occupied", 
-                "rental", "language", "citizen", "white", "low_income", "lone_parent", 
-                "university_education_pct_pop", "housing_need_pct_household", "non_mover_pct_pop", 
-                "owner_occupied_pct_household", "rental_pct_household", "language_pct_pop", 
-                "citizen_pct_pop", "white_pct_pop", "low_income_pct_pop", "lone_parent_pct_families"),
-      .funs = list(`z` = ~{(.- mean(., na.rm = TRUE))/sd(., na.rm = TRUE)})) 
-  
-  datalist[[n]] <- CTs_canada_temp
-  
-}
-
-CTs_canada = do.call(rbind, datalist)
-
-rm(CTs_canada_temp, datalist)
-
+# 
+# ########################################### 1 - CANADA #######################################
+# 
+# # 1.1 LIST CENSUS VARIABLES
+# variables_canada <- list_census_vectors("CA16")
+# 
+# # 1.2 IMPORT CENSUS VARIABLES FOR COUNTRY-WIDE AVERAGE
+# canada <-
+#   get_census(
+#     dataset = "CA16", regions = list(C = "Canada"),
+#     vectors = c("v_CA16_2398", "v_CA16_5078", "v_CA16_4888", "v_CA16_6695",
+#                 "v_CA16_4837", "v_CA16_4838", "v_CA16_524",
+#                 "v_CA16_3393", "v_CA16_3996", "v_CA16_2540", "v_CA16_497", "v_CA16_484"),
+#     geo_format = "sf") %>%
+#   st_transform(102009) %>%
+#   dplyr::select(GeoUID, Population, Households, contains("v_CA"))
+# 
+# names(canada) <-
+#   c("Geo_UID", "population", "households", "med_income",
+#     "university_education", "housing_need", "non_mover", "owner_occupied",
+#     "rental", "no_language", "citizen", "white", "low_income_pct_pop", "lone_parent",
+#     "families", "geometry")
+# 
+# canada <- canada %>%
+#   mutate(language = population - no_language)
+# 
+# canada <- canada %>%
+#   mutate(
+#     lone_parent_pct_families = lone_parent / families,
+#     low_income = low_income_pct_pop/100*population,
+#     low_income_pct_pop = low_income_pct_pop/100,
+#     language = population - no_language) %>%
+#   mutate_at(
+#     .vars = c("university_education", "non_mover",
+#               "language", "citizen", "white"),
+#     .funs = list(`pct_pop` = ~{. / population})) %>%
+#   mutate_at(
+#     .vars = c("housing_need", "owner_occupied", "rental"),
+#     .funs = list(`pct_household` = ~{. / households}))
+# 
+# 
+# # 1.3 IMPORT CENSUS VARIABLES FOR ALL CMAs
+# CMAs_canada <-
+#   get_census(
+#     dataset = "CA16", regions = list(C = "Canada"), level = "CMA",
+#     vectors = c("v_CA16_2398", "v_CA16_5078", "v_CA16_4888", "v_CA16_6695",
+#                 "v_CA16_4837", "v_CA16_4838", "v_CA16_524",
+#                 "v_CA16_3393", "v_CA16_3996", "v_CA16_2540", "v_CA16_497", "v_CA16_484"),
+#     geo_format = "sf") %>%
+#   st_transform(102009) %>%
+#   filter(Type == "CMA") %>%
+#   dplyr::select(GeoUID, CMA_name = name, Population, Households, contains("v_CA"))
+# 
+# CMAs_canada$CMA_name <- CMAs_canada$CMA_name %>%
+#   iconv(to = "ASCII//TRANSLIT") %>%
+#   str_replace("'", "") %>%
+#   str_replace("`", "")
+# 
+# names(CMAs_canada) <-
+#   c("CMA_UID", "CMA_name", "population", "households", "med_income",
+#     "university_education", "housing_need", "non_mover", "owner_occupied",
+#     "rental", "no_language", "citizen", "white", "low_income_pct_pop", "lone_parent",
+#     "families", "geometry")
+# 
+# CMAs_canada <- CMAs_canada%>%
+#   separate(CMA_name, into = c("CMA_name", NA), sep = "[(]") %>%
+#   mutate(
+#     lone_parent_pct_families = lone_parent / families,
+#     low_income = low_income_pct_pop/100*population,
+#     low_income_pct_pop = low_income_pct_pop/100,
+#     language = population - no_language) %>%
+#   mutate_at(
+#     .vars = c("university_education", "non_mover",
+#               "language", "citizen", "white"),
+#     .funs = list(`pct_pop` = ~{. / population})) %>%
+#   mutate_at(
+#     .vars = c("housing_need", "owner_occupied", "rental"),
+#     .funs = list(`pct_household` = ~{. / households}))
+# 
+# CMAs_canada$CMA_name <- substr(CMAs_canada$CMA_name, 1, nchar(CMAs_canada$CMA_name) - 1)
+# 
+# # 1.4 IMPORT CENSUS VARIABLES FOR ALL CENSUS TRACTS
+# CTs_canada <-
+#   get_census(
+#     dataset = "CA16", regions = list(C = "Canada"), level = "CT",
+#     vectors = c("v_CA16_2398", "v_CA16_5078", "v_CA16_4888", "v_CA16_6695",
+#                 "v_CA16_4837", "v_CA16_4838", "v_CA16_524",
+#                 "v_CA16_3393", "v_CA16_3996", "v_CA16_2540", "v_CA16_497", "v_CA16_484"),
+#     geo_format = "sf") %>%
+#   st_transform(102009) %>%
+#   filter(Type == "CT") %>%
+#   dplyr::select(GeoUID, PR_UID, CMA_UID, Population, Households, contains("v_CA"))
+# 
+# CTs_canada <- CTs_canada %>%
+#   inner_join(st_drop_geometry(dplyr::select(CMAs_canada, c(1,2))), by = "CMA_UID") %>%
+#  dplyr::select(GeoUID, CMA_UID, CMA_name, everything())
+# 
+# names(CTs_canada) <-
+#   c("Geo_UID", "CMA_UID", "CMA_name", "PR_UID", "population", "households", "med_income",
+#     "university_education", "housing_need", "non_mover", "owner_occupied",
+#     "rental", "no_language", "citizen", "white", "low_income_pct_pop",
+#     "lone_parent", "families", "geometry")
+# 
+# CTs_canada <- CTs_canada%>%
+#   mutate(
+#     lone_parent_pct_families = lone_parent / families,
+#     low_income = low_income_pct_pop/100*population,
+#     low_income_pct_pop = low_income_pct_pop/100,
+#     language = population - no_language) %>%
+#   mutate_at(
+#     .vars = c("university_education", "non_mover",
+#               "language", "citizen", "white"),
+#     .funs = list(`pct_pop` = ~{. / population})) %>%
+#   mutate_at(
+#     .vars = c("housing_need", "owner_occupied", "rental"),
+#     .funs = list(`pct_household` = ~{. / households})) %>%
+#   dplyr::select(-c("no_language"))
+# 
+# 
+# # 1.5 Z SCORES
+# datalist = list()
+# 
+# for (n in c(1:nrow(CMAs_canada))) {
+# 
+#   CTs_canada_temp <- CTs_canada %>%
+#     filter(CMA_UID == as.numeric(st_drop_geometry(CMAs_canada[n, 1]))) %>%
+#     mutate_at(
+#       .vars = c("population", "households", "med_income",
+#                 "university_education", "housing_need", "non_mover", "owner_occupied",
+#                 "rental", "language", "citizen", "white", "low_income", "lone_parent",
+#                 "university_education_pct_pop", "housing_need_pct_household", "non_mover_pct_pop",
+#                 "owner_occupied_pct_household", "rental_pct_household", "language_pct_pop",
+#                 "citizen_pct_pop", "white_pct_pop", "low_income_pct_pop", "lone_parent_pct_families"),
+#       .funs = list(`z` = ~{(.- mean(., na.rm = TRUE))/sd(., na.rm = TRUE)}))
+# 
+#   datalist[[n]] <- CTs_canada_temp
+# 
+# }
+# 
+# CTs_canada = do.call(rbind, datalist)
+# 
+# rm(CTs_canada_temp, datalist)
+# 
 
 ########################################### 2 - UNITED STATES #####################################
 
@@ -414,12 +413,21 @@ CTs_us$geometry <- st_cast(CTs_us$geometry, "MULTIPOLYGON")
 
 ########################################### 3 - COMBINE CENSUS TRACTS #####################################
 
+# CTs <- 
+# rbind(CTs_canada %>% 
+#         mutate(ST_UID = PR_UID,
+#                country = "Canada") %>%
+#         dplyr::select(-"PR_UID") %>% 
+#         st_transform(102009), 
+#       CTs_us %>% 
+#         mutate(country = "US") %>% 
+#         st_transform(102009))
+
 CTs <- 
-rbind(CTs_canada %>% 
-        mutate(ST_UID = PR_UID,
-               country = "Canada") %>%
-        dplyr::select(-"PR_UID") %>% 
-        st_transform(102009), 
-      CTs_us %>% 
-        mutate(country = "US") %>% 
-        st_transform(102009))
+  CTs_us %>% 
+  mutate(country = "US") %>% 
+  st_transform(102009)
+
+
+
+
