3d351e7bcce8325ea732b9eaf159eb09105cfd97
amy <amy.bolt@mail.mcgill.ca>
2020-03-01 15:31:16 -0500

    glendale

--- R/05_media_import.R
+++ R/05_media_import.R
@@ -2,6 +2,30 @@
 
 source("R/01_helper_functions.R")
 
+# Update cityname files for import to account for the two Glendales
+cityname  <- c("Albuquerque", "Anaheim", "Anchorage", "Arlington", "Atlanta", 
+               "Aurora", "Austin", "Bakersfield", "Baltimore",
+               "Baton Rouge", "Birmingham", "Boise", "Boston",
+               "Buffalo", "Cincinnati", "Chandler", "Charlotte", "Chesapeake",
+               "Chicago", "Chula Vista", "Cleveland", "Colorado Springs", 
+               "Columbus", "Corpus Christi",  "Dallas", "Denver", "Des Moines", 
+               "Detroit", "Durham", "El Paso", "Fayetteville", "Fontana", 
+               "Fort Wayne", "Fort Worth", "Fremont", "Fresno", "Garland", 
+               "Gilbert", "Glendale", "Glendale", "Grand Rapids", 
+               "Greensboro", "Henderson", "Hialeah", "Honolulu", "Houston", 
+               "Huntington Beach", "Indianapolis", "Irvine", "Irving", "Jacksonville", 
+               "Jersey City", "Kansas City", "Laredo", "Las Vegas", "Lexington", 
+               "Lincoln", "Long Beach", "Los Angeles", "Louisville", "Lubbock", 
+               "Madison", "Memphis", "Mesa", "Miami", "Milwaukee", "Minneapolis", "Modesto", 
+               "Moreno Valley", "Nashville", "New Orleans", "New York", "Newark",
+               "Norfolk", "North Las Vegas", "Oakland", "Oklahoma City", "Omaha",
+               "Orlando", "Oxnard", "Philadelphia", "Phoenix", "Pittsburgh", "Plano", 
+               "Portland", "Raleigh", "Reno", "Richmond", "Riverside", "Rochester", 
+               "Sacramento", "Saint Louis", "Saint Paul", "Saint Petersburg", "Salt Lake City", 
+               "San Antonio", "San Bernardino", "San Diego", "San Francisco", "San Jose", 
+               "Santa Ana", "Santa Clarita", "Scottsdale", "Seattle", "Spokane", "Stockton", 
+               "Tacoma", "Tampa", "Toledo", "Tucson", "Tulsa", "Virginia Beach", "Washington", 
+               "Wichita", "Winston Salem")
 # Import factiva and lexisnexis files
 
 media <- 
