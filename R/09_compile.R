5be251ced7cb95b334492b206404a1392ee148d2
amy <amy.bolt@mail.mcgill.ca>
2020-03-20 10:51:24 -0400

    remove country field

--- R/09_compile.R
+++ R/09_compile.R
@@ -2,27 +2,25 @@
 
 source("R/01_helper_functions.R")
 
-# Cast geometries as polygons and add city naem as a variable
+# Cast geometries as polygons and add city name as a variable
 
-neighbourhoods_cast <- map(seq_along(neighbourhoods), ~{
-  neighbourhoods[[.x]] %>% 
-    st_cast("MULTIPOLYGON") %>% 
-    mutate(city = cityname[[.x]])
-})
+neighbourhoods <-
+  map2(neighbourhoods, cityname, ~{
+    .x %>% 
+      mutate(geometry = st_union(st_collection_extract(geometry, "MULTIPOLYGON")),
+             city = .y)
+  })
 
-# Create a datatable from the neighbourhood list
-neighbourhoods_table <- rbindlist(neighbourhoods_cast) 
-
-# Add country field
-neighbourhoods_table <- 
-  st_join(neighbourhoods_table %>% 
-            st_as_sf() %>% 
-            st_transform(102009), nation() %>% 
-                                  st_as_sf() %>% 
-                                  st_transform(102009) %>% 
-                                  dplyr::select(NAME), 
-         left = TRUE) %>% 
-  mutate(country = ifelse(is.na(NAME), "Canada", "United States")) %>% 
-  dplyr::select(-NAME)
- 
-
+# # Add country field
+# neighbourhoods_table <- 
+#   st_join(neighbourhoods_table %>% 
+#             st_as_sf() %>% 
+#             st_transform(102009), nation() %>% 
+#                                   st_as_sf() %>% 
+#                                   st_transform(102009) %>% 
+#                                   dplyr::select(NAME), 
+#          left = TRUE) %>% 
+#   mutate(country = ifelse(is.na(NAME), "Canada", "United States")) %>% 
+#   dplyr::select(-NAME)
+#  
+# 
