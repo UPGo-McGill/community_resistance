e1c73fc43eba0e05d7f9a24e59f081de0dc5bbad
David Wachsmuth <david.wachsmuth@mcgill.ca>
2020-04-04 11:18:04 -0400

    Cleaned up script 4

--- R/04_census_analysis.R
+++ R/04_census_analysis.R
@@ -1,22 +1,26 @@
-######################################### CENSUS ANALYSIS ###############################
+##### 04. CENSUS ANALYSIS ######################################################
 
 source("R/01_helper_functions.R")
 
-# Use st_intersect_summarize to estimate values from census tracts at the neighbourhood scale
+# Use st_intersect_summarize to estimate CT values at the neighbourhood scale
 neighbourhoods <- 
-map(neighbourhoods, ~{
+  map(neighbourhoods, ~{
   st_intersect_summarize(
     CTs,
     .x,
     group_vars = vars(neighbourhood),
     population = population,
-    sum_vars = vars(households, university_education, housing_need, non_mover, owner_occupied,
-                    rental, language, citizen, white, low_income, lone_parent, families),
-    mean_vars = vars(med_income, population_z, households_z, med_income_z, university_education_z,
-                     housing_need_z, non_mover_z, owner_occupied_z, rental_z, language_z,
-                     citizen_z, white_z, low_income_z, lone_parent_z, university_education_pct_pop_z, 
-                     housing_need_pct_household_z, non_mover_pct_pop_z, owner_occupied_pct_household_z,
-                     rental_pct_household_z, language_pct_pop_z, citizen_pct_pop_z, white_pct_pop_z, 
+    sum_vars = vars(households, university_education, housing_need, non_mover, 
+                    owner_occupied, rental, language, citizen, white, 
+                    low_income, lone_parent, families),
+    mean_vars = vars(med_income, population_z, households_z, med_income_z, 
+                     university_education_z, housing_need_z, non_mover_z, 
+                     owner_occupied_z, rental_z, language_z, citizen_z, white_z, 
+                     low_income_z, lone_parent_z, 
+                     university_education_pct_pop_z, 
+                     housing_need_pct_household_z, non_mover_pct_pop_z, 
+                     owner_occupied_pct_household_z, rental_pct_household_z, 
+                     language_pct_pop_z, citizen_pct_pop_z, white_pct_pop_z, 
                      low_income_pct_pop_z, lone_parent_pct_families_z)) %>% 
     ungroup() %>% 
     drop_units() 
@@ -48,4 +52,6 @@ neighbourhoods <-
              lone_parent = lone_parent * population,
              lone_parent_pct_families = lone_parent/(families * population))
   })
+
+rm(CTs)
 
